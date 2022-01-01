9f698bd084a81ddf4af9ec42df010c7d40c7bbc7
loglizzy <72479668+loglizzy@users.noreply.github.com>
2022-01-01 12:15:29 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,5 @@
-local sv = setmetatable({},{
-	__index = function(self, ind)
-		if ypcall(function()game:GetService(ind)end) then
-			return game:GetService(ind)
-		else
-			return nil
-		end
-	end
-})
-
 local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/Elerium-lib/main/lib.lua'))()
-local repl = sv.ReplicatedStorage
+local repl = game.ReplicatedStorage
 local a = require(repl.Aero.Shared.GameSettings.ModeData)
 a = getupvalue(a,1)
 
